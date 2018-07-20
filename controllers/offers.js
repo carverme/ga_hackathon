79c80b83d81285ea564bc0c786e03769658c1112
shullmb <shullmb@gmail.com>
2018-07-20 11:21:18 -0700

    added get routes

--- controllers/offers.js
+++ controllers/offers.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data/data.json');
 
+// GET /offers
 router.get('/', (req, res) => {
-  res.send('OFFERS')
+  // let offers = JSON.parse(fs.readFileSync(data));
+  res.render('/offers/index');
+
 })
+
+// POST /offers
+router.post('/', (req, res) => {
+
+})
+
+// GET /offers/new
+router.get('/:id', (req, res) => {
+  res.render('/offers/new')
+})
+
+// GET /offers/:id
+router.get('/:id', (req, res) => {
+  res.render('/offers')
+})
+
+// PUT /offers/:id
+
+
+// DELETE /offers/:id
+
+
 
 module.exports = router;
--- controllers/requests.js
+++ controllers/requests.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+// GET /requests
 router.get('/', (req, res) => {
-  res.send('Requests')
+  // let requests = JSON.parse(fs.readFileSync(data));
+  res.render('/requests/index');
+
+})
+
+// POST /requests
+router.post('/', (req, res) => {
+
+})
+
+// GET /requests/new
+router.get('/:id', (req, res) => {
+  res.render('/requests/new')
+})
+
+// GET /requests/:id
+router.get('/:id', (req, res) => {
+  res.render('/requests')
 })
 
 module.exports = router;
