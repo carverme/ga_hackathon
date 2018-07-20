35637d1ca059a8f83219227b1b322fec6425a74e
Matt Carver <mattcarver.wd@gmail.com>
2018-07-20 12:53:32 -0700

    updated ejs

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,4 @@ console.log("jscript working");
 $(document).ready(function() {
   console.log("jquery hitting")
   $(".dropdown-trigger").dropdown()
-  $('#textarea1').val('New Text');
-  M.textareaAutoResize($('#textarea1'));
 });
